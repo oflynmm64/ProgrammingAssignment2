044af8d1457f5f5f88d6c47481d292d36bfa167f
oflynmm64 <45844845+oflynmm64@users.noreply.github.com>
2019-12-09 18:12:31 +1100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions intend to obtain the inverse of a matrix,
+## and potentilly reduce the time taken to do so by caching previous ansers
 
-## Write a short comment describing this function
+## makeVector creates a function stored in a vector
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeVector <- function(x = numeric()) {
+  invers <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinvers <- function(inverse) invers <<- inverse
+  getinvers <- function() inv
+  list(set = set, get = get,
+       setinvers = setinvers,
+       getinvers = getinvers)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cachecalc checks if the inversion has already been performed 
+## if it has, then the cached reslut is used, saving computing time, electrical power and preserving our planet
+## if not, then the matrix is inverted to obtain a result
+cachecalc <- function(x, ...) {
+  inv <- x$getinvers()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrx <- x$get()
+  inv <- mean(matrx, ...)
+  x$setinvers(inv)
+  inv
 }
